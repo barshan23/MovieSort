6afd623baef7a8c6793a11dcc6eafbe8338443ee
barshan23 <barshan23@gmail.com>
2017-06-11 15:28:36 +0530

    comments are added

--- ratings.py
+++ ratings.py
@@ -22,25 +22,25 @@ for name in os.listdir("."):
 		movie = ''.join([i if (i != '.') else ' ' for i in movie ])# getting rid of the dots in the title
 		print movie
 		movie = movie.lower()
-		movie_search = '+'.join(movie.split())
-		url = base_url+movie_search
+		movie_search = '+'.join(movie.split())# exchanging the spaces with '+' sign
+		url = base_url+movie_search# constructing the search url
 		response = br.open(url) 
 		soup = BeautifulSoup(response.read(),'html.parser')
-		if soup.find("td",class_="result_text"):
-			links = soup.find("td",class_="result_text").find("a")
+		if soup.find("td",class_="result_text"):#finding the first table row with class 'result_text' that holds the first search reslult which is in most cases the desired movie
+			links = soup.find("td",class_="result_text").find("a")# now getting the link to the searched movie to get the rating
 			href = "http://www.imdb.com"+links.get("href")+"&s=tt&ttype=ft&ref_=fn_ft"
 			print href
 			page = br.open(href)
 			rating = BeautifulSoup(page.read(),'html.parser')
-			if rating.find("span",itemprop="ratingValue"):
-				ratings = rating.find("span",itemprop="ratingValue").getText()
+			if rating.find("span",itemprop="ratingValue"):#selecting the span that contains the rating of the movie
+				ratings = rating.find("span",itemprop="ratingValue").getText()# retrieving the rating value
 				movies[movie] = float(ratings) if (ratings) else 0
 				print ratings
 			else :
 				print "rating not found!! check the name"
 		else :
 			print "movie not found!!"
-d_view = [ (v,k) for k,v in movies.iteritems() ]
+d_view = [ (v,k) for k,v in movies.iteritems() ] # entering all the values from the dict to a list of tuples
 d_view.sort(reverse=True) # natively sort tuples by first element
 for v,k in d_view:
     print "%s: %f" % (k,v)
