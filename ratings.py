a633423f30f1fdf8f3a6f0bcfd7fdf499a6e019f
Avishek Saha <barshan23@gmail.com>
2017-09-19 11:33:05 +0530

    performace improvements and uncaught exceptions are gone nowgit status

--- ratings.py
+++ ratings.py
@@ -1,5 +1,5 @@
 import os
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, SoupStrainer
 import urllib2
 import mechanize
 
@@ -7,6 +7,7 @@ base_url = "http://www.imdb.com/find?ref_=nv_sr_fn&q="# base url for searching t
 br = mechanize.Browser()
 br.addheaders = [('User-agent','Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:53.0) Gecko/20100101 Firefox/53.0')]
 movies = {}
+only_td_tags = SoupStrainer(class_="result_text")
 
 for name in os.listdir("."):
 	movie = ''
@@ -23,22 +24,32 @@ for name in os.listdir("."):
 		movie = movie.lower()
 		movie_search = '+'.join(movie.split())# exchanging the spaces with '+' sign
 		url = base_url+movie_search# constructing the search url
-		response = br.open(url) 
-		soup = BeautifulSoup(response.read(),'html.parser')
-		if soup.find("td",class_="result_text"):#finding the first table row with class 'result_text' that holds the first search reslult which is in most cases the desired movie
-			links = soup.find("td",class_="result_text").find("a")# now getting the link to the searched movie to get the rating
-			href = "http://www.imdb.com"+links.get("href")+"&s=tt&ttype=ft&ref_=fn_ft"
-			print href
-			page = br.open(href)
-			rating = BeautifulSoup(page.read(),'html.parser')
-			if rating.find("span",itemprop="ratingValue"):#selecting the span that contains the rating of the movie
-				ratings = rating.find("span",itemprop="ratingValue").getText()# retrieving the rating value
-				movies[movie] = float(ratings) if (ratings) else 0
-				print ratings
+		try:
+			response = br.open(url)
+		except Exception as e:
+			print e
+		else:		
+			soup = BeautifulSoup(response.read(),'lxml', parse_only=only_td_tags)
+			link = soup.find("td",class_="result_text")
+			if link:#finding the first table row with class 'result_text' that holds the first search reslult which is in most cases the desired movie
+				links = link.find("a")# now getting the link to the searched movie to get the rating
+				href = "http://www.imdb.com"+links.get("href")+"&s=tt&ttype=ft&ref_=fn_ft"
+				print href
+				try:
+					page = br.open(href)
+				except Exception as e:
+					print e
+				else:
+					rating = BeautifulSoup(page.read(),'lxml',parse_only=SoupStrainer(itemprop="ratingValue"))
+					val = rating.find("span",itemprop="ratingValue")
+					if val:#selecting the span that contains the rating of the movie
+						ratings = val.getText()# retrieving the rating value
+						movies[movie] = float(ratings) if (ratings) else 0
+						print ratings
+					else :
+						print "rating not found!! check the name"
 			else :
-				print "rating not found!! check the name"
-		else :
-			print "movie not found!!"
+				print "movie not found!!"
 d_view = [ (v,k) for k,v in movies.iteritems() ] # entering all the values from the dict to a list of tuples
 d_view.sort(reverse=True) # natively sort tuples by first element
 for v,k in d_view:
